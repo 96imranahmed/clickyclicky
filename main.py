480405df6c59fbf2cde8350c34966dd88d458112
Imran Ahmed <96imranahmed@gmail.com>
2018-01-20 14:57:22 +0000

    Enable disable click

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from pynput import mouse
+from pymouse import PyMouse
+from pynput import mouse as PynputMouse
+import _thread
+import time
+
+stupid_global = False
 
 def on_move(x, y):
     print('Pointer moved to {0}'.format(
@@ -17,9 +22,30 @@ def on_scroll(x, y, dx, dy):
         'down' if dy < 0 else 'up',
         (x, y)))
 
-# Collect events until released
-with mouse.Listener(
+def ret_false(msg, data):
+    global l, stupid_global
+    if stupid_global:
+        if msg == 514 or msg == 513:
+            l.suppress_event()
+    return False
+
+def timer_please():
+    global stupid_global
+    while True:
+        time_now = time.time()
+        while (time.time() - time_now < 5):
+            pass
+        stupid_global = not stupid_global
+        print(stupid_global)
+
+l = PynputMouse.Listener(
         on_move=on_move,
         on_click=on_click,
-        on_scroll=on_scroll) as listener:
-    listener.join()
+        on_scroll=on_scroll,
+        win32_event_filter = ret_false)
+
+l.start()
+_thread.start_new_thread(timer_please,())
+l.join()
+
+
