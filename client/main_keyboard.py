2215ba9d16e1bbca78906aba9c044fcac93f027f
Imran Ahmed <96imranahmed@gmail.com>
2018-01-21 02:27:07 +0000

    Keyboard main code

--- client/main_keyboard.py
+++ client/main_keyboard.py
@@ -1,29 +1,35 @@
 from pynput import keyboard
+import pynput.keyboard._win32 as w32
 import _thread
 import time
 
+LST = w32.Listener(None, None)
 stupid_global = False
-
 
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        pass
+        # print('alphanumeric key {0} pressed'.format(key.char))
     except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        pass
+        # print('special key {0} pressed'.format(key))
 
 def on_release(key):
-    print('{0} released'.format(
-        key))
+    pass
+    # print('{0} released'.format(key))
     if key == keyboard.Key.esc:
         # Stop listener
         return False
 
-
-def ret_false(msg, data):
-    global l, stupid_global
+def preprocess_keys(msg, data):
+    global l, stupid_global, LST
     if stupid_global:
+        is_packet = data.vkCode == LST._VK_PACKET
+        if is_packet:
+            tup = (msg | LST._UTF16_FLAG, data.scanCode)
+        else:
+            tup = (msg, data.vkCode)
+        key_send = LST._event_to_key(tup[0], tup[1])
         l.suppress_event()
     return True
 
@@ -39,7 +45,7 @@ def timer_please():
 l = keyboard.Listener(
         on_press=on_press,
         on_release=on_release,
-        win32_event_filter = ret_false
+        win32_event_filter = preprocess_keys
         )
 
 l.start()
